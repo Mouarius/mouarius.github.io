131d90240b9c93bf184edebcb8f3963ce3a3a5df
mouarius <mouarius@gmail.com>
2014-06-28 15:52:08 +0200

    I have now the pretty entire backbone
    , needs the content now

--- js/script.js
+++ js/script.js
@@ -4,5 +4,18 @@ $(document).ready(function(){
         var prev = $(element).prev();
         return prev.css("color");
     }
+    $(".pure-u-1").click(function(){
+        var variable = 100-33.3333;
+        var child = $(this).find(".custom-box");
+        var hasClass = $(this).hasClass(".is-activated")
+        if(hasClass){
+            $(this).css("paddingRight",0);
+
+        }
+        else{
+            $(this).css("paddingRight",variable + "%");
+        }
+        $(this).toggleClass(".is-activated");
+    });
 
 });
