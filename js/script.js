973524e0a419b127ac3ab83f48d3b4019e70b1f6
mouarius <mouarius@gmail.com>
2014-06-29 19:12:56 +0200

    Some modifications

--- js/script.js
+++ js/script.js
@@ -7,15 +7,26 @@ $(document).ready(function(){
     $(".pure-u-1").click(function(){
         var variable = 100-33.3333;
         var child = $(this).find(".custom-box");
-        var hasClass = $(this).hasClass(".is-activated")
+        var hasClass = $(this).hasClass("is-activated")
         if(hasClass){
-            $(this).css("paddingRight",0);
-
+            $(".pure-u-1").show();
+            $(this).css("width","33.3333%");
         }
         else{
-            $(this).css("paddingRight",variable + "%");
+            $(".pure-u-1").hide();
+            $(this).show();
+            $(this).css("left", "33.3333%");
+            $(this).css("margin", "auto");
+            $(this).animate({
+                width: "100%",
+                paddingBottom: "20em"
+            }, 500);
         }
-        $(this).toggleClass(".is-activated");
+        $(this).toggleClass("is-activated");
+        $(this).toggleClass("box");
+        
+        
     });
+    
 
 });
